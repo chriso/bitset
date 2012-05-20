35e86e528db6b999c484df8a8b2d219a5eadda6d
Chris O'Hara <cohara87@gmail.com>
2012-05-20 16:35:32 +1000

    Use linear counting by default

--- lib/vector.c
+++ lib/vector.c
@@ -7,6 +7,7 @@
 #include "bitset/bitset.h"
 #include "bitset/vector.h"
 #include "bitset/operation.h"
+#include "bitset/probabilistic.h"
 
 bitset_vector *bitset_vector_new() {
     bitset_vector *l = (bitset_vector *) malloc(sizeof(bitset_vector));
@@ -290,22 +291,19 @@ void bitset_vector_iterator_concat(bitset_vector_iterator *i, bitset_vector_iter
 void bitset_vector_iterator_count(bitset_vector_iterator *i, unsigned *raw, unsigned *unique) {
     unsigned raw_count = 0, offset;
     bitset *b;
-    bitset_operation *o;
-    if (unique) {
-        o = bitset_operation_new(NULL);
-    }
     BITSET_VECTOR_FOREACH(i, b, offset) {
         raw_count += bitset_count(b);
-        if (unique) {
-            bitset_operation_add(o, b, BITSET_OR);
-        }
     }
     (void)offset;
     *raw = raw_count;
     if (unique) {
-        *unique = bitset_operation_count(o);
+        bitset_linear *l = bitset_linear_new(raw_count);
+        BITSET_VECTOR_FOREACH(i, b, offset) {
+            bitset_linear_add(l, b);
+        }
+        *unique = bitset_linear_count(l);
+        bitset_linear_free(l);
     }
-    bitset_operation_free(o);
 }
 
 bitset_vector *bitset_vector_iterator_compact(bitset_vector_iterator *i) {
