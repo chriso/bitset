bca7f72e1b0071b2bf3c33dcec31b3c2c1859bea
Chris O'Hara <cohara87@gmail.com>
2012-05-24 08:59:41 +1000

    Remove loglog prototypes for now

--- include/bitset/probabilistic.h
+++ include/bitset/probabilistic.h
@@ -18,16 +18,6 @@ typedef struct bitset_linear_ {
 } bitset_linear;
 
 /**
- * Bitset HyperLogLog type.
- */
-
-typedef struct bitset_loglog_ {
-    bitset_word *words;
-    unsigned count;
-    size_t size;
-} bitset_loglog;
-
-/**
  * Estimate unique bits using an uncompressed bitset of the specified size
  * (bloom filter where n=1).
  */
@@ -51,31 +41,6 @@ unsigned bitset_linear_count(bitset_linear *);
  */
 
 void bitset_linear_free(bitset_linear *);
-
-/**
- * Estimate unique bits using the efficient HyperLogLog method.
- * See: algo.inria.fr/flajolet/Publications/FlFuGaMe07.pdf
- */
-
-bitset_loglog *bitset_loglog_new(size_t);
-
-/**
- * Estimate unique bits in the bitset.
- */
-
-void bitset_loglog_add(bitset_loglog *, bitset *);
-
-/**
- * Get the estimated unique bit count.
- */
-
-unsigned bitset_loglog_count(bitset_loglog *);
-
-/**
- * Free the loglog counter.
- */
-
-void bitset_loglog_free(bitset_loglog *);
 
 #ifdef __cplusplus
 } //extern "C"
